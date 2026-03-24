f15ea36c5e3102094241a4858b5d30e095a9e32b
rishurathour11 <rishurathour1449@gmail.com>
2026-03-24 21:05:23 +0530

    Add a simple addition program in sum.py

---
+++ sum.py
@@ -0,0 +1,11 @@
+# Addition Program
+
+# taking input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# adding numbers
+sum = num1 + num2
+
+# displaying result
+print("The sum is:", sum)
