259f350ba0406dc526055fc7a1372cc613ac142a
rishurathour11 <rishurathour1449@gmail.com>
2026-03-24 21:11:53 +0530

    Create multiply.py

---
+++ multiply.py
@@ -0,0 +1,11 @@
+# Multiplication Program
+
+# taking input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# multiplying numbers
+result = num1 * num2
+
+# displaying result
+print("The result is:", result)
